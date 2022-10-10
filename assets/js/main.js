24f468980aba716eeb830a8ce0ef8013b1256a71
sgraziola <simo.fdl@gmail.com>
2022-10-10 15:57:05 +0200

    Ex FizzBuzz: Add Bonus 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,20 +10,40 @@
 */
 
 
-//1. Scrivi un programma che stampi in console i numeri da 1 a 100
-
+//Scrivi un programma che stampi in console i numeri da 1 a 100
+const containerEl = document.querySelector(".container");
 for (let i=1; i<=100; i++){
     if (i%3 == 0 && i%5 == 0) {
         //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
-        console.log("FizzBuzz");
+        //console.log("FizzBuzz");
+        const divEl = document.createElement("div");
+        divEl.append("FizzBuzz");
+        divEl.classList.add("fizzbuzz");
+        containerEl.append(divEl);
     } else if (i%5 == 0) {
         //per i multipli di 5 stampi “Buzz”.
-        console.log("Buzz"); 
+        //console.log("Buzz"); 
+        const divEl = document.createElement("div");
+        divEl.append("Buzz");
+        divEl.classList.add("buzz");
+        containerEl.append(divEl);
     } else if (i%3 == 0){
         //Sostituisco ai multipli di 3 "Fizz"
-        console.log("Fizz");
+        //console.log("Fizz");
+        const divEl = document.createElement("div");
+        divEl.append("Fizz");
+        divEl.classList.add("fizz");
+        containerEl.append(divEl);
     } else {
-        console.log(i);      
+        const divEl = document.createElement("div");
+        //console.log(i);      
+        divEl.append(`${i}`);
+        divEl.classList.add("box");        
+        containerEl.append(divEl);
     }
 }
+/* BONUS 1:
+Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare. */
 
+/* BONUS 2:
+Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
